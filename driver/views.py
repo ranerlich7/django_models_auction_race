c605bbef602612004d40313ce468b7904745e83e
ran <ranerlich@gmail.com>
2023-07-28 12:55:35 +0300

    added a simple view for all drivers

--- driver/views.py
+++ driver/views.py
@@ -1,6 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from driver.models import Driver
 
 # Create your views here.
 
 # def index():
     # Driver.objects.all()
+    
+def drivers(request):
+    # get all drivers from DB
+    all_drivers = Driver.objects.all()
+    mystr = ""
+    for driver in all_drivers:
+        # add driver str
+        mystr += str(driver)
+        # add all races for this driver - driver.race_set.all()
+        mystr += str([str(race) for race in driver.race_set.all()])
+        mystr += "<br>"
+    # return all drivers as html
+    return HttpResponse(f"This is a list of drivers!<br>{mystr}")
+    
